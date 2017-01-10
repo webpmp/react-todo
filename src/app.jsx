ce9b5b793998a93ead2f9b88c746e4a390417b85
Shiva / John <hotline.shiva@gmail.com>
2017-01-11 02:42:15 +0545

    Bootstrap setup and beautification of application
    - Used the command `npm i react-bootstrap --save`
      ```bash
                       todo@1.0.0 /home/john/projects/reactjs/todo
                       └─┬ react-bootstrap@0.30.7
                         ├── classnames@2.2.5
                         ├── dom-helpers@2.4.0
                         ├── keycode@2.1.8
                         ├── react-overlays@0.6.10
                         ├── react-prop-types@0.4.0
                         ├── uncontrollable@4.0.3
                         └── warning@3.0.0
    
                       npm WARN optional Skipping failed optional dependency /chokidar/fsevents:
                       npm WARN notsup Not compatible with your operating system or architecture: fsevents@1.0.17
                       npm WARN todo@1.0.0 No repository field.
      ```
      - one should always restart the server after installing a new library; In other words--after package.json file is updated
      - FontAwesome installation
      - Commonly usable spacers.scss added to the repo

--- src/app.jsx
+++ src/app.jsx
@@ -4,13 +4,15 @@ import TodoList from './components/todo-list';
 
 // Simplest way to embed you stylesheets in your app
 //  requiring in main JS file is enough for whole application
+require ('./styles/spacer.scss');
 require ('./styles/index.scss');
 
 export default class App extends React.Component {
   render() {
     return (
         <div>
-          <h1> To do app in React.js</h1>
+          <h2>ToDo Manager</h2>
+          <h4>Manage your small reminders</h4>
           <TodoList />
         </div>
 
